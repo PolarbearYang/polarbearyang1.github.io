f7f144945bfa09efda013fbc42da971392bc19d9
PolarbearYang <1434367219@qq.com>
2021-04-22 22:34:11 +0800

    test

--- test.js
+++
@@ -1,8 +0,0 @@
-var myArray = new Array();
-myArray[2] = "true"
-myArray.push("penguin")
-myArray.unshift("polarbear")
-for (var i in myArray){
-    document.writeln("Index: " + i + ": " + myArray[i] + "<br>")
-}
-document.writeln(String("1"/0))
